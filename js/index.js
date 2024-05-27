6eac04fd133c46af5d4b99a1ee7c291023d6adbb
Dmytro Khodatskyi <khodatskyidmytro@gmail.com>
2024-05-27 21:04:56 +0300

    add FullPage.js library

---
+++ js/index.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    new fullpage('#fullpage', {
+        autoScrolling: true,
+        scrollHorizontally: true,
+        navigation: true,
+        navigationPosition: 'right',
+        licenseKey: 'gplv3-license'
+    });
+});
